b64dc8c4cffa0f35c2d7a35d3a217bdc31e637a1
Bastien Carré <bastien.carre@esigetel.fr>
2012-11-07 14:41:33 +0100

    Removed unused imports

--- Blue_weasel/src/com/bweasel/blue_weasel/MainActivity.java
+++ Blue_weasel/src/com/bweasel/blue_weasel/MainActivity.java
@@ -2,18 +2,15 @@ package com.bweasel.blue_weasel;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
